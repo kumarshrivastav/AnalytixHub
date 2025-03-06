af837e8cd6d4fa681eefb340b49d20d6c1269d47
ANKIT KUMAR <ankitkumarjl361@gmail.com>
2025-03-06 20:30:15 +0530

    redis.config.js updated

--- config/redis.config.js
+++ config/redis.config.js
@@ -1,23 +1,19 @@
-import {createClient} from "redis"
-import dotenv from "dotenv"
-dotenv.config({})
-console.log(process.env.REDIS_URL)
-const ConnectRedis=async()=>{
-    try {
-        const redisClient=createClient({url:process.env.REDIS_URL})
-        redisClient.on("error",(err)=>console.log(`Error Occured While Connecting to Redis:${err}`))
-        redisClient.on("ready",()=>console.log(`Redis Connected Successfully`))
-        await redisClient.connect()
-        await redisClient.ping()
-        return redisClient
-    } catch (error) {
-        throw error
-    }
+import { createClient } from 'redis';
+const redisConnect=async () => {
+  try {
+    const client = createClient({
+        url: process.env.REDIS_URL
+    });
+  
+    client.on('error', (err) => console.log('Redis Client Error', err));
+    client.on("ready",()=>console.log("Redis Connected Successfully"))
+    await client.connect();
+    await client.ping()
+    return client
+  } catch (error) {
+    throw new Error(error)
+  }
 }
 
-let RedisClient=await ConnectRedis()
-
-export const closeRedis=async()=>{
-    await RedisClient.quit()
-}
+let RedisClient=await redisConnect()
 export default RedisClient
